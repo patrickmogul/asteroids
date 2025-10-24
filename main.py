878eb7e6ef10d0c5c2c27e58948000e770ce3bdc
patrickmogul <patrick@easynode.pro>
2025-10-24 14:12:38 -0500

    Add CircleShape and Player classes for game object representation

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import pygame
 import constants
+from player import Player
 
 pygame.init()
 
 def main():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    # Create player in the center of the screen
+    x = constants.SCREEN_WIDTH / 2
+    y = constants.SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        
+        # Draw the player
+        player.draw(screen)
+        
         pygame.display.flip()
+        dt = clock.tick(60) / 1000  # Delta time in seconds.
+        
 if __name__ == "__main__":
     main()
